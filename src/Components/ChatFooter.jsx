53b7259651bf2fdd0198fce8d4f8f6da7fb5bb45
unknown <VISHNUKUgit>
2023-12-13 22:18:42 +0530

    13-12

---
+++ src/Components/ChatFooter.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const ChatFooter = ({socket}) => {
+  const [message, setMessage] = useState('');
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    console.log(socket.id);
+    if (message.trim() && localStorage.getItem('userName')) {
+        // Emit a 'message' event to the server using the socket
+        socket.emit('message', {
+          text: message,
+          name: localStorage.getItem('userName'),
+          id: `${socket.id}${Math.random()}`,  // Unique message ID
+          socketID: socket.id,  // Socket ID of the sender
+        });
+      }
+      // Clear the input field after sending a message
+      setMessage('');
+    // console.log({ userName: localStorage.getItem('userName'), message });
+    
+  };
+  return (
+    <div className="chat__footer">
+      <form className="form" onSubmit={handleSendMessage}>
+        <input
+          type="text"
+          placeholder="Write message"
+          className="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button className="sendBtn">SEND</button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatFooter;
